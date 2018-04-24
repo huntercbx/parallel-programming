2c6054110b138141a7266125654f6d4f28217199
Alexander Alexeyev <huntercbx@gmail.com>
2018-04-24 11:01:50 +0300

    Copy missing OpenCV dll (required to process video)

--- Exercises/03 - Parallel Pipeline/VS 2015/copy-libs.cmd
+++ Exercises/03 - Parallel Pipeline/VS 2015/copy-libs.cmd
@@ -1,6 +1,7 @@
 echo "Copying OpenCV dlls"
 xcopy /Y %1\x64\vc14\bin\opencv_world341.dll %3
 xcopy /Y %1\x64\vc14\bin\opencv_world341d.dll %3
+xcopy /Y %1\x64\vc14\bin\opencv_ffmpeg341_64.dll %3
 
 echo "Copying TBB dlls"
 xcopy /Y %2\bin\intel64\vc14\tbb.dll %3
